0faac5578d95d62b7793808810955ceed3512861
Steve C <stevencarofano@gmail.com>
2021-09-06 19:21:12 -0400

    Tweaking server file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget_tracker_db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
